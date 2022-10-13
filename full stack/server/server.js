71c8a8b2aab107580e9c6d5a4857eb8888ce856f
unknown <qazxwd44@hanmail.net>
2022-10-13 23:21:14 +0900

    mongo 연결

--- full stack/server/server.js
+++ full stack/server/server.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const uri =
+  "mongodb+srv://bogus:1234@cluster0.ghdzd.mongodb.net/?retryWrites=true&w=majority";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+}
+connect();
 
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
